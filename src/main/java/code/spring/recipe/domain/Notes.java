8b783cfe90cbeab6a456bfcff85a129d99bd6686
Roger <roger.puchaicela@njcode.es>
2022-07-27 13:27:41 +0200

    Creation entitys

---
+++ src/main/java/code/spring/recipe/domain/Notes.java
@@ -0,0 +1,41 @@
+package code.spring.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob//permite poner mas de 255 caracteres en la base de datos
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
